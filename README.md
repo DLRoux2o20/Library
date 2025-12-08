59ddaf3595f04eae3369126f2b9e02dba89ccdf7
DLRoux2o20 <153112792+DLRoux2o20@users.noreply.github.com>
2025-12-08 20:27:17 +0200

    Make form checkbox style and text change when clicked. Make all inputs left-aligned. Add lessons to README file.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Library
+Lessons:
+
+1. To create a custom icon for the content of a ::before or ::after, use url() for a downloaded image of the icon or use the unicode of the icon form websites like Font Awesome, but put a \ before the unicode and inside everything "".
--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-// let addBookButton = document.getElementById("add-book-button");
+let addBookButton = document.getElementById("add-book-button");
+let readCheckbox = document.getElementById("read");
+let readCheckboxText = document.getElementById("checkbox-span");
+
+readCheckbox.addEventListener("click", function() {
+    if (readCheckbox.checked) {
+        readCheckboxText.textContent = "Read";
+    } else {
+        readCheckboxText.textContent = "Not Read";
+    }
+})
 
 // addBookButton.addEventListener("click", function() {
 
