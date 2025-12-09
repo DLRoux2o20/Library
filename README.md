ebf0e4de2521bf610e3de750e8088b0bcf65df9e
DLRoux2o20 <153112792+DLRoux2o20@users.noreply.github.com>
2025-12-09 10:03:06 +0200

    Add lesson to README file. Add hr and submit button to form. Make form close button and add book button work by changing display of form between none and block. Change size of form div.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Lessons:
 
 1. To create a custom icon for the content of a ::before or ::after, use url() for a downloaded image of the icon or use the unicode of the icon form websites like Font Awesome, but put a \ before the unicode and inside everything "".
+2. Margin: auto; only works on elements with a display value of block.
--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 let addBookButton = document.getElementById("add-book-button");
+let form = document.querySelector("form");
 let readCheckbox = document.getElementById("read");
 let readCheckboxText = document.getElementById("checkbox-span");
+let closeFormButton = document.getElementById("form-close-div");
 
-readCheckbox.addEventListener("click", function() {
-    if (readCheckbox.checked) {
-        readCheckboxText.textContent = "Read";
-    } else {
-        readCheckboxText.textContent = "Not Read";
-    }
-})
+readCheckbox.addEventListener("click", function () {
+	if (readCheckbox.checked) {
+		readCheckboxText.textContent = "Read";
+	} else {
+		readCheckboxText.textContent = "Not Read";
+	}
+});
 
-// addBookButton.addEventListener("click", function() {
+closeFormButton.addEventListener("click", closeForm);
 
-// })
+function closeForm() {
+    form.style.display = "none";
+}
+
+addBookButton.addEventListener("click", function openForm() {
+    form.style.display = "block";
+});
