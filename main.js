698177ba26c4a040430c1344618017aad3ab2da2
DLRoux2o20 <153112792+DLRoux2o20@users.noreply.github.com>
2025-12-10 12:08:15 +0200

    Add object constructor and function to add it to an array. Submitted information in the form is used to create an object.

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ let titleInput = document.getElementById("title");
 let authorInput = document.getElementById("author");
 let pagesInput = document.getElementById("pages");
 
+const myLibrary = [];
+
 readCheckbox.addEventListener("click", function () {
 	if (readCheckbox.checked) {
 		readCheckboxText.textContent = "Read";
@@ -36,5 +38,26 @@ form.addEventListener("submit", submitForm);
 
 function submitForm(event) {
 	event.preventDefault();
+	let title = titleInput.value;
+	let author = authorInput.value;
+	let pages = pagesInput.value;
+	let read = readCheckbox.checked;
+	addBookToLibrary(title, author, pages, read);
 	closeForm();
+}
+
+function Book(title, author, pages, read) {
+	if (!new.target) {
+		throw Error ("Must use 'new' operator to call constructor");
+	}
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+	let book = new Book(title, author, pages, read);
+	myLibrary.push(book);
+	console.log(myLibrary);
 }
